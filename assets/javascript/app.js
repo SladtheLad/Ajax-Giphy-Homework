1b8fc750e374fd9e7d28261a9b0cc6006de06a27
nickoliasxii <nickoliasxii@gmail.com>
2017-10-24 17:16:44 -0500

    initial stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -49,11 +49,51 @@ $("#add-movie").on("click", function (event) {
 
 });
 
-// Function for displaying the movie info
-// We're adding a click event listener to all elements with the class "movie"
-// We're adding the event listener to the document because it will work for dynamically generated elements
-// $(".movies").on("click") will only add listeners to elements that are on the page at that time
-$(document).on("click", ".movie", alertMovieName);
+// Adding click event listen listener to all buttons
+$("button").on("click", function () {
+    // Grabbing and storing the data-animal property value from the button
+    var movie = $(this).attr("data-name");
+
+    // Constructing a queryURL using the animal name
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        movie + "&api_key=R1AvYMPhRU32sllboEiuXybJM9CJydmO&limit=10";
+
+    // Performing an AJAX request with the queryURL
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        // After data comes back from the request
+        .done(function (response) {
+            console.log(queryURL);
+
+            console.log(response);
+            // storing the data from the AJAX request in the results variable
+            var results = response.data;
+
+            // Looping through each result item
+            for (var i = 0; i < results.length; i++) {
+
+                // Creating and storing a div tag
+                var movieDiv = $("<div>");
+
+                // Creating a paragraph tag with the result item's rating
+                var p = $("<p>").text("Rating: " + results[i].rating);
+
+                // Creating and storing an image tag
+                var movieImage = $("<img>");
+                // Setting the src attribute of the image to a property pulled off the result item
+                movieImage.attr("src", results[i].images.fixed_height.url);
+
+                // Appending the paragraph and image tag to the animalDiv
+                movieDiv.append(p);
+                movieDiv.append(movieImage);
+
+                // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+                $("#gifs-appear-here").prepend(movieDiv);
+            }
+        });
+});
 
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
